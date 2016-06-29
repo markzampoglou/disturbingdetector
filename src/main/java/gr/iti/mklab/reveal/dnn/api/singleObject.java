4c591e2d7601fac08cdf33871d95ef28989a4688
markzampoglou <markzampoglou@gmail.com>
2016-06-29 15:26:00 +0300

    docker ready

---
+++ src/main/java/gr/iti/mklab/reveal/dnn/api/singleObject.java
@@ -0,0 +1,11 @@
+package gr.iti.mklab.reveal.dnn.api;
+
+import org.mongodb.morphia.annotations.Entity;
+
+/**
+ * Created by marzampoglou on 6/28/16.
+ */
+@Entity
+public class singleObject {
+    public float prediction=-10000;
+}
--- src/main/java/gr/iti/mklab/reveal/web/DisturbingDetector.java
+++ src/main/java/gr/iti/mklab/reveal/web/DisturbingDetector.java
@@ -2,10 +2,12 @@ package gr.iti.mklab.reveal.web;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import com.google.gson.JsonObject;
 import com.mongodb.MongoClient;
 import gr.iti.mklab.reveal.dnn.api.QueueObject;
 import gr.iti.mklab.reveal.dnn.api.ReportManagement;
 
+import gr.iti.mklab.reveal.dnn.api.singleObject;
 import gr.iti.mklab.reveal.util.Configuration;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.MessageDigest;
 
 
@@ -24,9 +31,9 @@ import java.security.MessageDigest;
 public class DisturbingDetector {
     private final String USER_AGENT = "Mozilla/5.0";
 
-      public DisturbingDetector() throws Exception {
-          Configuration.load(getClass().getResourceAsStream("/remote.properties"));
-      }
+    public DisturbingDetector() throws Exception {
+        Configuration.load(getClass().getResourceAsStream("/docker.properties"));
+    }
 
     // Suppress MongoDB logging
     static Logger root = (Logger) LoggerFactory
@@ -38,17 +45,63 @@ public class DisturbingDetector {
     @PreDestroy
     public void cleanUp() throws Exception {
         System.out.println("Spring Container destroy");
-      //  MorphiaManager.tearDown();
+        //  MorphiaManager.tearDown();
     }
 
     ///////////////////////////////////////////////////////
     /////////// DOWNLOAD IMAGE     ////////////////////////
     ///////////////////////////////////////////////////////
 
-
-    @RequestMapping(value = "/getfromurl", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/classify_violent", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public String getfromurl(@RequestParam(value = "url", required = true) String url){
+    public singleObject classify_violent(@RequestParam(value = "imageurl", required = true) String url){
+        System.out.println("synchronous URL disturbing detector demand received");
+        singleObject JSONResponse=new singleObject();
+        try {
+            String imgHash = null;
+            byte[] urlBytes = url.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.reset();
+            md.update(urlBytes);
+            byte[] digest=md.digest();
+            imgHash = String.format("%032x", new java.math.BigInteger(1, digest));
+            System.out.println("Hash : " + imgHash);
+
+            String filePath=ReportManagement.downloadURL(url, Configuration.QUEUE_IMAGE_PATH, imgHash);
+            System.out.println("Downloaded:");
+            System.out.println(filePath);
+
+            URL serviceUrl = new URL("http://localhost:5000/classify_violent?imagepath=" + filePath);
+            HttpURLConnection con = (HttpURLConnection) serviceUrl.openConnection();
+            // optional default is GET
+            con.setRequestMethod("GET");
+            //add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            //(new File(Configuration.QUEUE_IMAGE_PATH + imgHash + ".jpg")).delete();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            JSONResponse.prediction=Float.valueOf(response.toString());
+            return JSONResponse;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return JSONResponse;
+        }
+
+    }
+
+    @RequestMapping(value = "/getfromurltuqueue", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String getfromurltoqueue(@RequestParam(value = "url", required = true) String url){
         System.out.println("disturbing image detector initialized with URL");
         MongoClient mongoclient = new MongoClient(Configuration.MONGO_HOST, 27017);
         Morphia morphia = new Morphia();
@@ -122,7 +175,7 @@ public class DisturbingDetector {
 
         try {
             String imgHash = null;
-            byte[] urlBytes = url.getBytes("UTF-8");
+            byte[] urlBytes = (url+collection).getBytes("UTF-8");
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.reset();
             md.update(urlBytes);
@@ -132,23 +185,22 @@ public class DisturbingDetector {
 
 
             QueueObject queueItem = ds.get(QueueObject.class, imgHash);
-            while (queueItem != null && imgHash.length()<50) {
-                imgHash=imgHash+"_";
-                queueItem = ds.get(QueueObject.class, imgHash);
-            }
+            if (queueItem == null) {
 
-            try {ReportManagement.savebytearray(bytes, Configuration.QUEUE_IMAGE_PATH, imgHash);}
-            catch (Exception e) {e.printStackTrace();}
+                try {
+                    ReportManagement.savebytearray(bytes, Configuration.QUEUE_IMAGE_PATH, imgHash);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-            queueItem = new QueueObject();
-            queueItem.id=imgHash;
-            queueItem.processing=false;
-            queueItem.sourceURL=url;
-            queueItem.itemId=itemId;
-            queueItem.collection=collection;
-            queueItem.type=type;
-            ds.save(queueItem);
-            mongoclient.close();
+                queueItem = new QueueObject();
+                queueItem.id = imgHash;
+                queueItem.processing = false;
+                queueItem.sourceURL = url;
+                queueItem.itemId = itemId;
+                queueItem.collection = collection;
+                queueItem.type = type;
+                ds.save(queueItem);
             /*
             URL serviceUrl = new URL("http://localhost:5000/classify_violent?imagepath=" + downloadedFilePath);
             HttpURLConnection con = (HttpURLConnection) serviceUrl.openConnection();
@@ -171,7 +223,10 @@ public class DisturbingDetector {
 
             return response.toString();
             */
+            }
+            mongoclient.close();
             return "ok";
+
         } catch (Exception ex) {
             ex.printStackTrace();
             mongoclient.close();
